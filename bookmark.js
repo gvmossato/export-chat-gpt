8c7a62beda8f5a233eef90c22e52b9de444f4b96
gvmossato_loyall <gabriel@loyall.com.br>
2023-04-08 15:03:32 -0300

    code style and minor customizations

--- bookmark.js
+++ bookmark.js
@@ -1,35 +1,56 @@
 javascript: (function () {
+  function updateChatHeader(model, name) {
+    return `🧠 ${model.replace("Model: ", "")} | 💬 ${name}`;
+  }
+  function isAvatar(node) {
+    return (
+      (node.matches(".items-end") && node.querySelector("svg.h-6.w-6, img"))
+      || node.closest("svg")
+      || node.matches("svg.h-6.w-6")
+      || node.matches('img[alt*="@"]')
+    );
+  }
+  function b64Image(img) {
+    const canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    return canvas.toDataURL("image/png");
+  }
   try {
     const a = document.createElement("a");
     const dom = document.querySelector("main > .flex-1 > .h-full .flex");
     const template = document.createElement("template");
-    const user_image = dom.querySelector(".items-end img.rounded-sm");
-    const avatar_url = base64_image(user_image);
+    const userImage = dom.querySelector(".items-end img.rounded-sm");
+    const avatarUrl = b64Image(userImage);
     const title = document.title;
-    const non_letters_re = /[^\p{L}\p{N}]+/gu;
-    const trailing_dash_re = /(^-)|(-$)/g;
+    const modelVersionDiv = document.querySelector("main .w-full");
+    const nonLettersRegex = /[^\p{L}\p{N}]+/gu;
+    const trailingDashRegex = /(^-)|(-$)/g;
     const slug = title
       .toLowerCase()
-      .replace(non_letters_re, "-")
-      .replace(trailing_dash_re, "");
+      .replace(nonLettersRegex, "-")
+      .replace(trailingDashRegex, "");
+    modelVersionDiv.innerHTML = updateChatHeader(
+      modelVersionDiv.innerHTML,
+      title
+    );
     template.innerHTML = dom.innerHTML;
     ["button", "img[aria-hidden='true']"].forEach((selector) => {
       template.content.querySelectorAll(selector).forEach((node) => {
-        if (!node.closest(".math") && !is_avatar(node)) {
-          node.remove();
-        }
+        if (!node.closest(".math") && !isAvatar(node)) node.remove();
       });
     });
     template.content.querySelectorAll("img").forEach((node) => {
       node.setAttribute("alt", "user avatar");
-      node.setAttribute("src", avatar_url);
-      ["srcset", "style"].forEach((attr) => {
-        node.removeAttribute(attr);
-      });
+      node.setAttribute("src", avatarUrl);
+      ["srcset", "style"].forEach((attr) => node.removeAttribute(attr));
     });
     a.href = URL.createObjectURL(
       new Blob(
-        [`<!DOCTYPE html>
+        [
+          `<!DOCTYPE html>
           <html class="dark" style="color-scheme: dark;">
           <head>
             <title>Chat GPT | ${title}</title>
@@ -37,9 +58,11 @@ javascript: (function () {
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" />
             <link rel="stylesheet" href="https://rawcdn.githack.com/gvmossato/export-chat-gpt/feature/default-styles/styles/styles.css" type="text/css" />
           </head>
-          <body>${template.innerHTML}</body>
-          </html>
-        `],
+          <body>
+            ${template.innerHTML}
+          </body>
+          </html>`,
+        ],
         { type: "text/html" }
       )
     );
@@ -50,21 +73,5 @@ javascript: (function () {
     URL.revokeObjectURL(a.href);
   } catch (e) {
     alert(e.message);
-  }
-  function is_avatar(node) {
-    return (
-      (node.matches(".items-end") && node.querySelector("svg.h-6.w-6, img")) ||
-      node.closest("svg") ||
-      node.matches("svg.h-6.w-6") ||
-      node.matches('img[alt*="@"]')
-    );
-  }
-  function base64_image(img) {
-    const canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    return canvas.toDataURL("image/png");
   }
 })();
