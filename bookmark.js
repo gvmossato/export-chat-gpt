cffd3499facfa40cf703da91be24697de9cc171a
Gabriel Mossato <41247052+gvmossato@users.noreply.github.com>
2023-04-17 15:02:08 -0300

    fix(style-ref): avoid the branch name in styles url

--- bookmark.js
+++ bookmark.js
@@ -56,7 +56,7 @@ javascript: (function () {
             <title>Chat GPT | ${title}</title>
             <meta charset="utf-8"/>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" />
-            <link rel="stylesheet" href="https://rawcdn.githack.com/gvmossato/export-chat-gpt/feature/default-styles/styles/styles.css" type="text/css" />
+            <link rel="stylesheet" href="https://rawcdn.githack.com/gvmossato/export-chat-gpt/315fe8b317880d2a1fa210ba1da3ad435baad1c6/styles/styles.css" type="text/css" />
           </head>
           <body>
             ${template.innerHTML}
